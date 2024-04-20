0b1295e01dd96e277a8193b6dbbae2b4d65910c3
Sokrat Magomedov <oooosokrat@mail.ru>
2024-04-20 20:53:56 +0300

    1 test for products

--- market_case/payments/views.py
+++ market_case/payments/views.py
@@ -1,13 +1,10 @@
 from django.core.mail import send_mail
 from django.db.models import Sum, F
-from django.http import HttpResponse
-from django.shortcuts import render
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
-from cart.serializers import CartSerializer
-from payments.serializers import MailSerializer
 
 
 # Create your views here.
@@ -15,10 +12,11 @@ from payments.serializers import MailSerializer
 @api_view(['GET'])
 def send_mail_test(request):
     cart = request.user.cart
-    email1 = 'ooosokrat@mail.ru'
+    email1 = request.user.email
     url = 'http://test-payments.mediann-dev.ru/payment'
     query = cart.items.aggregate(sum=Sum(F('quantity') * F('product__discount_price')),
                                  quantity=Sum('quantity'))
+
     data = {
         'amount': query['sum'],
         'items_qty': query['quantity'],
@@ -29,13 +27,12 @@ def send_mail_test(request):
     response = response.json()
     subject = "Оплата"
     message = f"Ссылка: {response['url']}, номер заказа: {response['orderId']}"
-    recipient_list = ['ooosokrat@mail.ru']
+    recipient_list = [email1]
     send_mail(
         subject=subject,
         message=message,
         from_email=None,
-        recipient_list=recipient_list,
-        fail_silently=False)
+        recipient_list=recipient_list)
     return Response({
         'subject': subject,
         'message': message,
--- market_case/products/tests.py
+++ market_case/products/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from products.models import Products, Category, Subcategory
+from products.serializers import ProductSerializer
+
 
 # Create your tests here.
+
+class ProductsTest(APITestCase):
+    def test_get(self):
+        category_1 = Category.objects.create(category_name='Автомобили')
+        sub_category_1 = Subcategory.objects.create(subcategory_name='Седаны',
+                                                    parent_id=1)
+        product_1 = Products.objects.create(product_name='Тойота Камри',
+                                            price=6000, discount_price=5000,
+                                            product_balance=7, product_characteristics='Обычная гиря',
+                                            product_category_id=1, product_subcategory_id=1)
+
+        url = reverse('products:products')
+        res = self.client.get(url)
+        response_data = res.data.get('results')
+        self.assertEqual(ProductSerializer([product_1], many=True).data, response_data)
