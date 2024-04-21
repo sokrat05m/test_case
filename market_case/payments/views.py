fe6c19181c81fc2dd002accac406e6d0e3ebc112
Sokrat Magomedov <oooosokrat@mail.ru>
2024-04-21 23:46:07 +0300

    Payment view fixed

--- market_case/payments/views.py
+++ market_case/payments/views.py
@@ -8,22 +8,23 @@ from payments.utils import get_payment_link, check_item_quantity, clear_cart
 
 @api_view(['GET'])
 def send_mail_test(request):
-    response = get_payment_link(request)
-    subject = "Оплата"
-    message = f"Ссылка: {response['url']}, номер заказа: {response['orderId']}"
-    recipient_list = [request.user.email]
-    check = check_item_quantity(request)
-    if check:
-        return Response({'message': f'Продукта {check} нет в таком количестве'})
-    try:
-        send_mail(subject=subject,
-                  message=message,
-                  from_email=None,
-                  recipient_list=recipient_list,
-                  fail_silently=False)
-        clear_cart(request)
-        return Response({'message': 'Письмо успешно отправлено'})
-    except Exception as e:
-        return Response({'message': f'Ошибка с отправкой письма: {str(e)}'},
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    if request.user.cart.items.all():
+        response = get_payment_link(request)
+        subject = "Оплата"
+        message = f"Ссылка: {response['url']}, номер заказа: {response['orderId']}"
+        recipient_list = [request.user.email]
+        check = check_item_quantity(request)
+        if check:
+            return Response({'message': f'Продукта {check} нет в таком количестве'})
+        try:
+            send_mail(subject=subject,
+                      message=message,
+                      from_email=None,
+                      recipient_list=recipient_list,
+                      fail_silently=False)
+            clear_cart(request)
+            return Response({'message': 'Письмо успешно отправлено'})
+        except Exception as e:
+            return Response({'message': f'Ошибка с отправкой письма: {str(e)}'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({'message': 'Пустая корзина'})
