9978b0a5d5b7709002796a6506f6fef2c9b7f053
Sokrat Magomedov <oooosokrat@mail.ru>
2024-05-10 11:05:36 +0300

    task and payment view modified

--- market_case/payments/tasks.py
+++ market_case/payments/tasks.py
@@ -2,15 +2,12 @@ from django.core.mail import send_mail
 
 from cart.models import Cart
 from celery_app import app
-from payments.utils import check_item_quantity, get_payment_link, clear_cart
+from payments.utils import get_payment_link, clear_cart
 
 
 @app.task()
 def send_order_mail_task(cart_id: int, email: str) -> str:
     cart = Cart.objects.get(id=cart_id)
-    check = check_item_quantity(cart)
-    if check:
-        return f'Продукта {check} нет в таком количестве'
     if cart.items.exists():
         response = get_payment_link(cart, email)
         subject = "Оплата"
--- market_case/payments/views.py
+++ market_case/payments/views.py
@@ -6,13 +6,18 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .tasks import send_order_mail_task
+from .utils import check_item_quantity
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def send_order_detail_mail(request: Request) -> Response:
     email = request.user.email
-    cart_id = request.user.cart.id
-    print(email)
-    result = send_order_mail_task.delay(cart_id, email)
+    cart = request.user.cart
+    check = check_item_quantity(cart)
+    if check:
+        return Response(
+            {'message': f"Продукта '{check}' нет в таком количестве"}
+        )
+    result = send_order_mail_task.delay(cart.id, email)
     return Response({'message': result.get()})
